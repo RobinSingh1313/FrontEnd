241f4573ffcf7a9c63e7d05097fbdb2237fc3701
RobinSingh1313 <robinsingh13213@gmail.com>
2023-11-23 16:23:16 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Sidebar from './Component/Sidebar';
+// import SecondSidebar from './Component/SecondSidebar'; // Remove this line if not needed
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Sidebar />
+      {/* <SecondSidebar /> */}
+      {/* ^ Uncomment this line if you actually need to use it */}
+    </>
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+    theme: {
+    extend: {
+      colors: {
+        primary: '#FE6100', // Your primary color
+        secondary: '#FFDDC7', // Your secondary color
+        // Add more global colors as needed
+      },
+
+      
+    },
+  },
+  plugins: [],
+}
+
